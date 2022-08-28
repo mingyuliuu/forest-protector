68c7c213c16a7d60ded389fbd98377229a54d1b4
Mingyu Liu <mingyuliu88@gmail.com>
2022-08-27 22:55:04 -0400

    Implement Wildfire Details Section

--- src/App.js
+++ src/App.js
@@ -10,9 +10,96 @@ function App() {
   const fetchEvents = async () => {
     setIsLoading(true);
 
-    const res = await fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events?days=15");
-    const { events } = await res.json();
-
+    //const res = await fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events?days=15");
+    //const { events } = await res.json();
+    const { events } = 
+    {
+      "title": "EONET Events",
+      "description": "Natural events from EONET.",
+      "link": "https://eonet.gsfc.nasa.gov/api/v2.1/events",
+      "events": [
+        {
+          "id": "EONET_6194",
+          "title": "Bezymianny Volcano, Russia",
+                "description": "",
+          "link": "https://eonet.gsfc.nasa.gov/api/v2.1/events/EONET_6194",
+          "categories": [
+            {
+              "id": 12,
+              "title": "Volcanoes"
+            }
+          ],
+          "sources": [
+            {
+              "id": "SIVolcano",
+              "url": "http://volcano.si.edu/volcano.cfm?vn=300250"
+            }
+          
+          ],
+          "geometries": [
+            {
+              "date": "2022-08-22T10:30:34Z",
+              "type": "Point", 
+              "coordinates": [ 160.595, 55.972 ]
+            }
+          
+          ]
+        },
+        {
+          "id": "EONET_6194",
+          "title": "Crockets Knob Fire",
+                "description": "",
+          "link": "https://eonet.gsfc.nasa.gov/api/v2.1/events/EONET_6195",
+          "categories": [
+            {
+              "id": 8,
+              "title": "Wildfires"
+            }
+          ],
+          "sources": [
+            {
+              "id": "InciWeb",
+              "url": "http://inciweb.nwcg.gov/incident/8355/"
+            }
+          
+          ],
+          "geometries": [
+            {
+              "date": "2022-08-22T03:00:00Z",
+              "type": "Point", 
+              "coordinates": [ -118.708, 44.728000000000002 ]
+            }
+          
+          ]
+        },
+        {
+          "id": "EONET_6195",
+          "title": "AbaAba",
+                "description": "",
+          "link": "https://eonet.gsfc.nasa.gov/api/v2.1/events/EONET_6195",
+          "categories": [
+            {
+              "id": 8,
+              "title": "Wildfires"
+            }
+          ],
+          "sources": [
+            {
+              "id": "InciWeb",
+              "url": "http://inciweb.nwcg.gov/incident/8355/"
+            }
+          
+          ],
+          "geometries": [
+            {
+              "date": "2022-08-22T03:00:00Z",
+              "type": "Point", 
+              "coordinates": [ -118.708, 47.728000000000002 ]
+            }
+          
+          ]
+        },
+      ]};
     setEventData(events);
     setIsLoading(false);
   };
--- src/components/Map.js
+++ src/components/Map.js
@@ -5,6 +5,7 @@ import WildfireDetail from "./WildfireDetail";
 
 const Map = ({ eventData, center, zoom }) => {
   const [wildfireDetail, setWildfireDetail] = useState(null);
+  const [isShowingDetail, setIsShowingDetail] = useState(false);
 
   const wildfireMarkers =
     eventData === null
@@ -13,17 +14,27 @@ const Map = ({ eventData, center, zoom }) => {
           // 8 corresponds to Wildfires
           if (event.categories[0].id !== 8) return null;
 
+          console.log(event);
+
           return (
             <WildfireMarker
+              key={event.id}
               lat={event.geometries[0].coordinates[1]}
               lng={event.geometries[0].coordinates[0]}
-              onClick={() =>
-                setWildfireDetail({ id: event.id, title: event.title })
-              }
+              onClick={() => {
+                setWildfireDetail({
+                  id: event.id,
+                  title: event.title,
+                  link: event.sources[0].url,
+                  time: event.geometries[0].date,
+                });
+                setIsShowingDetail(true);
+              }}
             ></WildfireMarker>
           );
         });
 
+  // The default location when the user doesn't grant gps permission is set default to UW.
   const defaultProps = {
     center: {
       lat: 43.472286,
@@ -32,16 +43,36 @@ const Map = ({ eventData, center, zoom }) => {
     zoom: 8,
   };
 
+  // Close the details section on the right
+  const closeDetail = () => {
+    setIsShowingDetail(false);
+  };
+
   return (
-    <div className="map" style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAbaZ_2-PaTeYCZAHZcqPOfGUX42Ijsm0Q" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+    <div className="mapView">
+      {/* The map section that will take up 70% of screen width or 100% if details are not shown */}
+      <div
+        className="map"
+        style={{ height: "100vh", width: isShowingDetail ? "70vw" : "100vw" }}
       >
-        {wildfireMarkers}
-        {wildfireDetail && <WildfireDetail info={wildfireDetail} />}
-      </GoogleMapReact>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyAbaZ_2-PaTeYCZAHZcqPOfGUX42Ijsm0Q" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          {wildfireMarkers}
+        </GoogleMapReact>
+      </div>
+
+      {/* The details section (that will take up 30% of screen width) */}
+      <div
+        className="rightContainer"
+        style={{ height: "100vh", width: isShowingDetail ? "30vw" : "0vw" }}
+      >
+        {wildfireDetail && (
+          <WildfireDetail info={wildfireDetail} close={() => closeDetail()} />
+        )}
+      </div>
     </div>
   );
 };
