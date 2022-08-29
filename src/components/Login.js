8cd8a70439beaca09d4b2f439605b62a98b104e5
Mingyu Liu <mingyuliu88@gmail.com>
2022-08-29 00:13:58 -0400

    Add bg image for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,30 @@
 import React from "react";
 import Userfront from "@userfront/react";
+import BgImage from "../bg.png";
 
 Userfront.init("8nw8zw5b");
 
 const SignupForm = Userfront.build({
-  toolId: "anddmd"
+  toolId: "anddmd",
 });
 
 const Login = () => {
-  return <SignupForm/>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundImage: `url(${BgImage})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <SignupForm />
+    </div>
+  );
 };
 
 export default Login;
